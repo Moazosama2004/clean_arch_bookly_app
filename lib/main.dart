2a5f910b7201ac4e998ba0dc2d051ecb12f03643
Muaz osama <moaz21-01130@student.eelu.edu.eg>
2025-04-16 00:29:13 +0200

    generate book entity adapter

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,15 @@ import 'package:bookly/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 import 'Features/Splash/presentation/views/splash_view.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Hive
+  await Hive.initFlutter();
   runApp(const Bookly());
 }
 
