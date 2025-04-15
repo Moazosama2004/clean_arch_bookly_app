aaf0d1ff5e9786b33cc38fbaf2f796ecefc5cbaa
Muaz osama <moaz21-01130@student.eelu.edu.eg>
2025-04-15 21:24:33 +0200

    transform book model to book entity

--- lib/Features/home/domain/entities/book_entity.dart
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -1,16 +1,16 @@
 class BookEntity {
-  final String image;
+  final String bookId;
+  final String? image;
   final String title;
-  final String authorName;
-  final num price;
-  final num rating;
-  final num rateCount;
+  final String? authorName;
+  final num? price;
+  final num? rating;
 
   const BookEntity({
+    required this.bookId,
     required this.title,
     required this.authorName,
     required this.price,
-    required this.rateCount,
     required this.image,
     required this.rating,
   });
