7ad37e52f91d7b91fd9e34626067b9ea7b11edf8
Muaz osama <moaz21-01130@student.eelu.edu.eg>
2025-04-15 23:54:04 +0200

    create home remote data source

---
+++ lib/Features/home/data/data_sources/home_remote_data_source.dart
@@ -0,0 +1,26 @@
+import 'package:bookly/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly/core/utils/api_service.dart';
+
+abstract class HomeRemoteDataSource {
+  Future<List<BookEntity>> fetchFeaturedBooks();
+  Future<List<BookEntity>> fetchNewestBooks();
+}
+
+// class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
+//   final ApiService _apiService;
+
+//   HomeRemoteDataSourceImpl(this._apiService);
+//   @override
+//   Future<List<BookEntity>> fetchFeaturedBooks() async {
+//     var data = await _apiService.get(endPoint: '');
+//     List<BookEntity> books = [];
+//     for (var item in data) {
+//       books.add(BookModel.fromJson(item));
+//     }
+//     return books;
+//   }
+
+//   @override
+//   Future<List<BookEntity>> fetchNewestBooks() {}
+// }
