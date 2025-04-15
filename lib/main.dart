5d5c8f7b3fecc55d8d1bd3f78bd1228ff9d03050
Muaz osama <moaz21-01130@student.eelu.edu.eg>
2025-04-16 00:33:29 +0200

    register adapter and open featured box

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,18 @@
+import 'package:bookly/Features/home/domain/entities/book_entity.dart';
 import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-
-import 'Features/Splash/presentation/views/splash_view.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // Initialize Hive
   await Hive.initFlutter();
+  // register adapter
+  Hive.registerAdapter(BookEntityAdapter());
+  await Hive.openBox(kFeaturedBox);
   runApp(const Bookly());
 }
 
